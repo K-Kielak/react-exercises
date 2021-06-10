dfa9df3bff884185d0cc3d2a15d73d33338fdb81
Kacper Kielak <kielak.kacper@gmail.com>
2021-06-10 17:08:49 +0800

    Add initial seasons implementations

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,36 @@
-import React, { ReactElement } from "react"
+import React, { ReactNode } from "react"
 import ReactDOM from "react-dom"
 
-function App(): ReactElement {
-  return <div></div>
+import SeasonDisplay from "./SeasonDisplay"
+
+interface AppProps {}
+
+interface AppState {
+  lattitude: number | null
+  errorMessage: string | null
+}
+
+class App extends React.Component<AppProps, AppState> {
+  state: AppState = { lattitude: null, errorMessage: null }
+
+  componentDidMount(): void {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lattitude: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message }),
+    )
+  }
+
+  render(): ReactNode {
+    if (this.state.lattitude != null) {
+      return <div>Lattitude: {this.state.lattitude}</div>
+    }
+
+    if (this.state.errorMessage != null) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    return <div>Loading!</div>
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
